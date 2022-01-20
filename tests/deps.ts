266a0f5d72b3a7d299f182393a4c4eb462b90932
Hugo Caillard <hg.caillard@gmail.com>
2022-01-20 16:37:05 +0100

    upgrade dependencies

--- tests/deps.ts
+++ tests/deps.ts
@@ -2,10 +2,10 @@ export {
   Clarinet,
   Tx,
   types,
-} from 'https://deno.land/x/clarinet@v0.23.0/index.ts'
+} from 'https://deno.land/x/clarinet@v0.24.0/index.ts'
 export type {
   Chain,
   Account,
-} from 'https://deno.land/x/clarinet@v0.23.0/index.ts'
+} from 'https://deno.land/x/clarinet@v0.24.0/index.ts'
 
 export { assertEquals } from 'https://deno.land/std@0.120.0/testing/asserts.ts'
